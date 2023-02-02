0a0a5e52549ed78e858ad3b5bd851ffa15ca0020
cptemre <kunduraci2@gmail.com>
2023-02-02 12:38:10 +0100

    Project Upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# ALGORITHMS AND DATA STRUCTURES LABORATORY
+
+## TASK 8
+
+### GENERAL INFORMATION
+
+1. This project has been prepared for a course in informatics in the first semester.
+
+2. Project is prepared in Visual Studio 2022 application in Windows 11 OS. It may not work as intended in other platforms.
+
+3. Project only contains the code in github not the project folder prepared by Visual Studio.
+
+### DESCRIPTION OF THE TASK
+
+Create the algorithm for searching an array of n elements on the diagram.Values in the area are: 7,22,9,86,4,7,13,28,7,99 hard coded in the program code.
+Do the search sequentially in two variants:
+a) Search key - the result is the first found value equal to the key and its index
+b) Search for a key - the result is all the values ​​in the table corresponding to the key and theirs indexes.
---
+++ task-8.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int key = 0;
+    int index = 0;
+    vector<int> myList{ 7,22,9,86,4,7,13,28,7,99 };
+
+    cout << "a) Search key - the result is the first found value equal to the key and its index: \n";
+    cout << "Please enter a key to find its first index: ";
+    cin >> key;
+    cout << endl;
+
+    for (int i = 0; i < myList.size(); i++)
+    {
+        if (key == myList.at(i))
+        {
+            cout << "The key you search " << key << " is found at index position " << i << endl;
+            index = i;
+            break;
+        }
+        else
+        {
+            if (i == myList.size() - 1 && index == 0)
+            {
+                cout << "The key you search " << key << " is not in our list";
+            }
+        }
+    }
+
+    cout << "\nb) Search for a key - the result is all the values ​​in the table corresponding to the key and theirs indexes.";
+    cout << "\nPlease enter a key to find its indexes: ";
+    cin >> key;
+    index = 0;
+    cout << endl;
+
+    for (int i = 0; i < myList.size(); i++)
+    {
+        if (key == myList.at(i))
+        {
+            cout << "The key you search " << key << " is found at index position " << i << endl;
+            index = i;
+        }
+        else
+        {
+            if (i == myList.size() - 1 && index == 0)
+            {
+                cout << "The key you search " << key << " is not in our list";
+            }
+        }
+    }
+    return 0;
+}
